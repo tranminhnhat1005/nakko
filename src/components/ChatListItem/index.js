cda7ee0c0e23d24b8977a799d01808450ebdef70
Nhat Tran <tranminhnhat1005@gmail.com>
2022-11-27 15:31:13 +0700

    dev - ChatListItem props

--- src/components/ChatListItem/index.js
+++ src/components/ChatListItem/index.js
@@ -1,28 +1,22 @@
 import { Image, Text, View, StyleSheet } from 'react-native';
 import React from 'react';
 
-const data = {
-    uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg',
-    name: 'Nak',
-    time: '8:30',
-    message: 'Hello guy!',
-};
 const SZ_IMG = 60;
 const SZ_SPC = 10;
 
-const ChatListItem = () => {
+const ChatListItem = ({ data }) => {
     return (
         <View style={styles.viewContainer}>
-            <Image source={{ uri: data.uri }} style={styles.img} />
+            <Image source={{ uri: data.user.image }} style={styles.img} />
             <View style={styles.viewContent}>
                 <View style={styles.viewInfo}>
                     <Text numberOfLines={1} style={styles.txtName}>
-                        {data.name}
+                        {data.user.name}
                     </Text>
-                    <Text style={styles.txtTime}>{data.time}</Text>
+                    <Text style={styles.txtTime}>{data.lastMessage.createdAt}</Text>
                 </View>
                 <Text numberOfLines={2} style={styles.txtMessage}>
-                    {data.message}
+                    {data.lastMessage.text}
                 </Text>
             </View>
         </View>
