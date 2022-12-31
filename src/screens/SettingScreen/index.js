278cba83727939ffedd236e48f14401ea7c16a75
Nhat Tran <tranminhnhat1005@gmail.com>
2022-12-31 10:38:50 +0700

    backend - update settings screen

--- src/screens/SettingScreen/index.js
+++ src/screens/SettingScreen/index.js
@@ -1,12 +1,26 @@
-import { Button, StyleSheet, View } from 'react-native';
+import { Button, StyleSheet, View, Text } from 'react-native';
 import { Auth } from 'aws-amplify';
+import { useEffect, useState } from 'react';
 
 const SettingScreen = () => {
+    const [info, setInfo] = useState({});
+
+    useEffect(() => {
+        const getUserInfo = async () => {
+            const response = await Auth.currentAuthenticatedUser();
+            setInfo(response.attributes);
+        };
+
+        getUserInfo();
+    }, []);
+
     const onSignOut = () => {
         Auth.signOut();
     };
     return (
         <View style={styles.viewContainer}>
+            <Text>{info.email || ''}</Text>
+            <Text>{info.phone_number || ''}</Text>
             <Button style={styles.btnSignOut} title={'Sign Out'} onPress={onSignOut} />
         </View>
     );
@@ -19,6 +33,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
     btnSignOut: {
+        marginTop: 15,
         backgroundColor: 'green',
         color: 'white',
     },
